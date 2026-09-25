a63f2fe24b124bd68602644b47e711f42728c550
lucasflores-web <lucasrodriguessilvaflores@gmail.com>
2026-09-25 15:42:29 -0300

    adição da pagina de programação do evento

--- matrix.js
+++ matrix.js
@@ -33,7 +33,7 @@ function draw() {
   for (var i = 0; i < drops.length; i++) {
     var text = letters[Math.floor(Math.random() * letters.length)];
     ctx.fillStyle = 'rgb(13, 255, 0)';
-    //ctx.fillStyle = 'rgb(0, 68, 255)';
+    //ctx.fillStyle = 'rgb(5, 5, 255)';
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
     drops[i]++;
     if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
